a236c41ddc544459ba7fe9d7e6c39acc1bf788c9
K.Jayasuriya <vkjs2006@gmail.com>
2026-06-14 10:26:46 +0530

    Improved layouts, added localStorage feature

--- js/main.js
+++ js/main.js
@@ -16,23 +16,58 @@ themeButton.addEventListener("click", () => {
     }
 });
 // Elements
+let editIndex = null;
 const noOfNotes = 0;
 const noteForm = document.getElementById("note-form");
 const addButton = document.querySelector(".add-btn");
 const saveButton = document.querySelector(".save-btn");
 const cancelButton = document.querySelector(".cancel-btn");
 const notesContainer = document.querySelector(".notes-container");
-
+const titleInput = document.getElementById("titleId");
+const notesInput = document.getElementById("notesId");
 //Empty notes situation
 
 /* Object to store the notes */
-const notesArr = [{
-    title: "",
-    date: "",
-    notes: ""
-}];
+const notes = JSON.parse(localStorage.getItem("notes")) || [];
 
-localStorage.getItem("notes",);
+/* Rendering the notes from stored object to webpage */
+function renderNotes() {
+    notesContainer.innerHTML = "";
+    notes.forEach((note, index) => {
+        const noteCard = document.createElement("div");
+        noteCard.classList.add("notes-card");
+        noteCard.innerHTML = `
+        <div class="notes-top">
+            <h2>${note.title}</h2>
+            <button class="edit-btn">✏️ Edit</button>
+            <button class="delete-btn"></button>
+        </div>
+        <h4 class = "notes-date">Saved DateTime: ${note.date}</h4>
+        <p>${note.content}</p>
+        `;
+
+
+        /* DELETE */
+        noteCard.querySelector(".delete-btn").addEventListener("click", () => {
+            notes.splice(index, 1);
+            saveNotes();
+            renderNotes();
+        });
+
+        /* EDIT */
+        noteCard.querySelector(".edit-btn").addEventListener("click", () => {
+            editIndex = index;
+            titleInput.value = note.title;
+            notesInput.value = note.content;
+            noteForm.showModel();
+        });
+        notesContainer.appendChild(noteCard);
+    });
+}
+
+function saveNotes() {
+    localStorage.setItem("notes", JSON.stringify(notes));
+}
 // Open dialog
 addButton.addEventListener("click", () => {
     noteForm.showModal();
@@ -43,53 +78,41 @@ cancelButton.addEventListener("click", () => {
 });
 // Save note
 saveButton.addEventListener("click", () => {
-    const title = document.getElementById("titleId").value;
-    const content = document.getElementById("notesId").value;
-    if (title === "" || content === "") {
+
+    const title = titleInput.value.trim();
+    const content = notesInput.value.trim();
+
+    if (!title || !content) {
         alert("Please enter notes!");
         return;
     }
-    const noteCard = document.createElement("div");
-    const currentDate = new Date().toLocaleString();
-    noteCard.classList.add("notes-card");
-    noteCard.innerHTML = `
-        <div class="notes-top">
-            <h2>${title}</h2>
-            <button class="edit-btn">✏️ Edit</button>
-            <button class="delete-btn"></button>
-        </div>
-        <h4 class = "notes-date">Saved DateTime: ${currentDate}</h4>
-        <p>${content}</p>
-    `;
-    notesContainer.appendChild(noteCard);
-    // Delete logic
-    const deleteBtn = noteCard.querySelector(".delete-btn");
-    deleteBtn.addEventListener("click", () => {
-        noteCard.remove();
-    });
 
-    // Clear inputs
-    document.getElementById("titleId").value = "";
-    document.getElementById("notesId").value = "";
-    // Close dialog
+    const noteData = {
+        title,
+        content,
+        date: new Date().toLocaleString()
+    };
+
+    /* EDIT EXISTING NOTE */
+    if (editIndex !== null) {
+
+        notes[editIndex] = noteData;
+
+        editIndex = null;
+    }
+
+    /* CREATE NEW NOTE */
+    else {
+        notes.push(noteData);
+    }
+
+    saveNotes();
+
+    renderNotes();
+
+    titleInput.value = "";
+    notesInput.value = "";
+
     noteForm.close();
-
-
-    /* Edit Feature */
-    const editButton = document.querySelector(".edit-btn");
-    editButton.addEventListener("click", () => {
-        noteForm.showModal();
-        const newTitle = document.getElementById("titleId").value;
-        const newNotes = document.getElementById("notesId").value;
-        noteCard.innerHTML = `
-        <div class="notes-top">
-            <h2>${newTitle}</h2>
-            <button class="edit-btn">✏️ Edit</button>
-            <button class="delete-btn"></button>
-        </div>
-        <h4>Saved DateTime: ${currentDate}</h4>
-        <p>${newNotes}</p>
-    `;
-    });
-
 });
+renderNotes();
