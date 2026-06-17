7601c6953f288f5d8bcbe0988fd5c0c8909047ff
K.Jayasuriya <vkjs2006@gmail.com>
2026-06-17 16:51:44 +0530

    Added Login page and Sign up page

---
+++ js/login.js
@@ -0,0 +1,17 @@
+// Theme Toggle
+const themeButton = document.querySelector("#theme-toggle");
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    themeButton.textContent = "☀️ Light Mode";
+}
+themeButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        themeButton.textContent = "☀️ Light Mode";
+    }
+    else {
+        localStorage.setItem("theme", "light");
+        themeButton.textContent = "🌙 Dark Mode";
+    }
+});
