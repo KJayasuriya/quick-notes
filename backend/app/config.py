bad7d086abe5a27b042cc67493c45d4bbd795f55
K.Jayasuriya <vkjs2006@gmail.com>
2026-07-19 15:35:10 +0530

    Replaced configuration to postgreSQL

--- backend/app/config.py
+++ backend/app/config.py
@@ -14,9 +14,6 @@ class Config:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
     JWT_EXPIRE_MINUTES = int(os.getenv("JWT_EXPIRE_MINUTES"))
-    # Oracle
-    ORACLE_USER = os.getenv("ORACLE_USER")
-    ORACLE_PASSWORD = os.getenv("ORACLE_PASSWORD")
-    ORACLE_HOST = os.getenv("ORACLE_HOST")
-    ORACLE_PORT = int(os.getenv("ORACLE_PORT", 1521))
-    ORACLE_SERVICE = os.getenv("ORACLE_SERVICE")
+    
+    # Database
+    DATABASE_URL = os.getenv("DATABASE_URL")
