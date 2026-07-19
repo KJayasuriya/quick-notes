c0a8b43977e1320cddb1bccdf9a633d7cd319679
K.Jayasuriya <vkjs2006@gmail.com>
2026-07-19 15:41:06 +0530

    Replace oracle variables

--- backend/app/config.py
+++ backend/app/config.py
@@ -17,3 +17,8 @@ class Config:
     
     # Database
     DATABASE_URL = os.getenv("DATABASE_URL")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = int(os.getenv("DB_PORT"))
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
